d4e759aef7e3a1aff1c31c50581c67ce290bea1c
EduardoNuzWehbe <alu0101128153@ull.edu.es>
2019-10-03 13:22:28 +0100

    organizando el codigo de arbol y dejandolo mas limpio

--- src/com/company/Arbol.java
+++ src/com/company/Arbol.java
@@ -1,6 +1,9 @@
 package com.company;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Vector;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Arbol {
 
@@ -10,28 +13,52 @@ public class Arbol {
 
     public Arbol(int inicio, int fin) {
         setNodoInicial(0);
-        setNodoFinal(1);
+        setNodoFinal(0);
     }
-    public void crearArbol (Vector<Nodo> informacionArbol) {
+
+    public void crearArbol(Vector<Nodo> informacionArbol) {
         Vector<Integer> nodosAccesibles;
+        int nodoPadre;
         nodosAccesibles = informacionArbol.get(getNodoInicial()).nodosPosibles();
-        System.out.print("Nodos accesibles por el nodo inicial " + getNodoInicial() + ":" );
+        AtomicInteger costes = new AtomicInteger(0);
+        Boolean encontrado = false;
+
+
+        System.out.print("Nodos accesibles por el nodo inicial " + getNodoInicial() + ":");
+
+        impresionAccesibles(nodosAccesibles);
+        if (destinoFinal()) return;
+
+        int nodoActual = getNodoInicial();
+        encontrado = finalEncontrado(informacionArbol, nodosAccesibles, costes, nodoActual);
+        if(encontrado == true) {
+            System.out.println("Hemos encontrado el nodo objetivo el coste ha sido: " + costes + "y hemos llegado a traves del nodo: " );
+        }
+    }
+
+    private void impresionAccesibles(Vector<Integer> nodosAccesibles) {
         for (Integer nodoAccesible : nodosAccesibles) {
             System.out.print(nodoAccesible + " ");
         }
         System.out.println("Comenzamos a trabajar con el arbol");
-        if(getNodoInicial() == getNodoFinal()) {
+    }
+
+    private boolean destinoFinal() {
+        if (getNodoInicial() == getNodoFinal()) {
             System.out.println("Hemos llegado al destino y tenemos un coste 0");
-            return;
-        } else {
-            int costes = 0;
-            int nodoActual = getNodoInicial();
-            for (Integer nodoAccesible : nodosAccesibles) {
-                if(nodoAccesible == getNodoFinal()) {
-                    costes += informacionArbol.get(nodoActual).getCostesNodo(nodoAccesible);
-                }
+            return true;
+        }
+        return false;
+    }
+
+    private Boolean finalEncontrado(Vector<Nodo> informacionArbol, Vector<Integer> nodosAccesibles, AtomicInteger costes, int nodoActual) {
+        for (Integer nodoAccesible : nodosAccesibles) {
+            if (nodoAccesible == getNodoFinal()) {
+                costes.getAndAdd(informacionArbol.get(nodoActual).getCostesNodo(nodoAccesible));
+                return true;
             }
         }
+        return false;
     }
 
     public int getNodoInicial() {
@@ -49,6 +76,7 @@ public class Arbol {
     public void setNodoFinal(int nodoFinal) {
         this.nodoFinal = nodoFinal;
     }
+
     public int getNodoPadre() {
         return nodoPadre;
     }
